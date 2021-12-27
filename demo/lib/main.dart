706d3c01f257d3cf4191a236bb201de60f75ac65
vishma97 <94741452+vishma97@users.noreply.github.com>
2021-12-27 16:28:28 +0530

    login screen ui (#9)

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,30 +1,34 @@
+import 'package:demo/views/home.dart';
+import 'package:demo/views/login.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:demo/commons/route_generator.dart';
 import 'package:demo/commons/themes.dart';
-import 'package:flutter/material.dart';
-import 'package:demo/views/home.dart';
-import 'package:demo/views/users.dart';
-import 'package:demo/views/user_profile.dart';
-import 'package:demo/commons/themes.dart';
+import 'package:demo/commons/utilities.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en', 'US'), Locale('de', 'DE')],
+        path: 'assets/langs',
+        fallbackLocale: Locale('en', 'US'),
+        child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      theme: primaryTheme,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
-      theme: lightTheme,
-      //theme: ThemeData(
-      //  primarySwatch: Colors.blue,
-     // ),
-      home: HomePage(title: "Home"),
-     // home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: Utilities.isLoggedIn() ? HomePage() : LoginPage(),
     );
   }
 }
