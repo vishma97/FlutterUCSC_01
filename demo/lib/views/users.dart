41c4263b3e532b2c9b963dcd4c5a4ec9494c23d6
Vishma Madurapperuma <vishmamadurapperuma123@gmail.com>
2022-01-01 09:49:49 +0530

    dev/users-screen-ui

--- demo/lib/views/users.dart
+++ demo/lib/views/users.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:demo/widgets/appbar.dart';
 import 'package:easy_localization/easy_localization.dart';
-//import 'package:demo/views/cards/user_card.dart';
+import 'package:demo/views/cards/user_card.dart';
 
 class UsersPage extends StatefulWidget {
   @override
@@ -9,56 +9,30 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
-
-final List<String> usernames = <String>[
+  List<String> users = [
     "Kamal Perera",
     "Nimal Dissanayaka",
     "Saman De Silva",
     "Sunil Mendis",
     "Shehan Gunawardana",
     "Sudath Nishantha"
-];
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Users'),
-      ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: usernames.length,
-          itemBuilder: (BuildContext context, int index){
-            return Container(
-              height: 150,
-              margin: EdgeInsets.all(15),
-           child: Row(
-                  children: <Widget>[
-                    Image(
-                        image: AssetImage("assets/images/login.jpg")),
-                    Container(
-                      margin: EdgeInsets.only(left: 15),
-                      child: Text(usernames[index] + '\nNo 2, Hill St, Kandy'),
-                    )
-                  ],
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 7,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
+        appBar: appBar('titles.users'.tr(), context),
+        body: ListView.separated(
+            itemBuilder: (BuildContext context, index) {
+              // print(stories[index]);
+              return userItem(context, users[index]);
+            },
+            separatorBuilder: (BuildContext context, index) {
+              return Divider(
+                height: 2.0,
+                color: Colors.transparent,
               );
-            }));
+            },
+            itemCount: users.length));
   }
 }
