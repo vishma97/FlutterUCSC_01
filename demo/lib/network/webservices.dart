6a208834a06ccc57e0c518231e5618cba4eaf30b
vishma97 <94741452+vishma97@users.noreply.github.com>
2022-01-05 16:21:30 +0530

    dev/industry-list-web-service (#15)

--- demo/lib/network/webservices.dart
+++ demo/lib/network/webservices.dart
@@ -1,3 +1,4 @@
+import 'package:demo/commons/utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:connectivity/connectivity.dart';
 import 'dart:convert';
@@ -7,6 +8,8 @@ import 'package:demo/widgets/flushbar.dart';
 import 'package:demo/commons/globals.dart' as global;
 import 'package:demo/models/login_response.dart';
 import 'package:demo/widgets/loader.dart';
+import 'package:demo/models/industry_response.dart';
+import 'package:demo/commons/utilities.dart';
 
 Future<LoginResponse> login(BuildContext context, username, password) async {
   var connectivityResult = await (Connectivity().checkConnectivity());
@@ -32,7 +35,44 @@ Future<LoginResponse> login(BuildContext context, username, password) async {
         return null;
       }
     } catch (e) {
-      showFloatingFlushbar(context, 'errors.login'.tr());
+      showFloatingFlushbar(context, 'errors.unknown'.tr());
+      return null;
+    }
+  } else {
+    showFloatingFlushbar(context, 'errors.network'.tr());
+    return null;
+  }
+}
+
+Future<IndustryResponse> getIndustries(BuildContext context) async {
+  var connectivityResult = await (Connectivity().checkConnectivity());
+  var token = await Utilities.userToken();
+
+  if ((connectivityResult == ConnectivityResult.mobile) ||
+      (connectivityResult == ConnectivityResult.wifi)) {
+    try {
+      showLoader(context);
+      final http.Response response =
+          await http.get(global.INDUSTRIES, headers: {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer $token',
+      });
+      var responseBody = json.decode(response.body);
+      stopLoader(context);
+      if (response.statusCode == 200) {
+        return IndustryResponse.fromJson(responseBody);
+      } else if (response.statusCode == 403) {
+        showFloatingFlushbar(context, 'errors.industries'.tr());
+        var isDelete = await Utilities.clearPreferences();
+        Navigator.pushNamed(context, '/login');
+        return null;
+      } else {
+        showFloatingFlushbar(context, 'errors.industries'.tr());
+        return null;
+      }
+    } catch (e) {
+      print(e);
+      showFloatingFlushbar(context, 'errors.unknown'.tr());
       return null;
     }
   } else {
