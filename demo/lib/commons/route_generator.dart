8ee6e6a02dff0a68babfdc4bee4e4ea81988db7b
Vishma Madurapperuma <vishmamadurapperuma123@gmail.com>
2022-01-05 16:13:31 +0530

    dev/industry-list-web-service

--- demo/lib/commons/route_generator.dart
+++ demo/lib/commons/route_generator.dart
@@ -1,7 +1,7 @@
 import 'package:demo/views/login.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/views/home.dart';
-import 'package:demo/views/users.dart';
+import 'package:demo/views/industries.dart';
 import 'package:demo/views/user_profile.dart';
 
 class RouteGenerator {
@@ -11,14 +11,12 @@ class RouteGenerator {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => HomePage());
-      case '/login':
-        return MaterialPageRoute(builder: (_) => LoginPage());
       case '/home':
         return MaterialPageRoute(builder: (_) => HomePage());
       case '/login':
         return MaterialPageRoute(builder: (_) => LoginPage());
-      case '/users':
-        return MaterialPageRoute(builder: (_) => UsersPage());
+      case '/industries':
+        return MaterialPageRoute(builder: (_) => IndustriesPage());
       case '/profile':
         return MaterialPageRoute(builder: (_) => ProfilePage());
       default:
